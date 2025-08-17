9b7504bb091d0c56ccf202aa7b117943b60f3adf
Shubham <shubhambiswas9899@gmail.com>
2025-08-17 19:26:45 +0530

    merchant order

--- Backend/src/routes/merchant.routes.js
+++ Backend/src/routes/merchant.routes.js
@@ -3,7 +3,7 @@ import upload , {handleMulterError} from '../middleware/multer.js'
 import { addBaseProduct , addVariant, getBaseProducts, getVariants,getCategories, addBrand, getBrands, getBaseProductById , updateVariant, getProductsByMerchantId, uploadProductImage, deleteImage} from '../controllers/merchantController/product.controllers.js';
 import { addMerchant } from '../controllers/merchantController/merchant.controller.js';
 import { registerMerchant, loginMerchant } from '../controllers/merchantController/authControllers.js';
-import {getOrdersForMerchant,orderPacked} from '../controllers/userControllers/order.controllers.js';
+import {getOrderForMerchant,orderPacked} from '../controllers/userControllers/order.controllers.js';
 import {authMiddlewareMerchant} from '../middleware/jwtAuth.js';
 const router = express.Router();
 
@@ -25,7 +25,7 @@ router.get('/getCategories',getCategories);
 router.post("/upload/image",upload.array("images", 5), handleMulterError,uploadProductImage );
 router.delete('/deleteImage/:imageId', deleteImage);
 
-router.get('/getOrders',authMiddlewareMerchant,getOrdersForMerchant);
+router.get('/getOrders',authMiddlewareMerchant,getOrderForMerchant);
 // router.post('/updateOrderStatus',updateOrderStatus);
 router.post('orderPacked',authMiddlewareMerchant,orderPacked)
 
